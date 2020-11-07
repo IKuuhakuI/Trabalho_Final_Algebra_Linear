3326153bd4617624d630915aee1d5df76617dc93
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-11-07 11:23:21 -0300

    adiciona a funcao que faz a matriz transposta

--- testeFormatar.py
+++ testeFormatar.py
@@ -1,4 +1,5 @@
 from libFormatar import LerCsv, SepararTipos, FormatarMatriz
+from libMatriz import GerarMatrizTransposta
 
 # TESTES ########
 matrizOrigem = []
@@ -13,15 +14,17 @@ SepararTipos (matrizOrigem, matriz1, matriz2, matriz3)
 
 matrizIndependente1 = []
 matrizDependente1 = []
+matrizTransposta1 = []
 
 FormatarMatriz (matriz1, matrizIndependente1, matrizDependente1) 
+GerarMatrizTransposta (matriz1, matrizTransposta1)
 
 print ("Matriz 1:")
 print (matriz1)
-print("\nMatriz 1 formatada: ")
+
+print ("\nMatriz 1 formatada: ")
 print (matrizIndependente1)
 print (matrizDependente1)
-print ()
-print (matriz2)
-print ()
-print (matriz3)
+
+print ("\nMatriz 1 transposta: ")
+print (matrizTransposta1)
