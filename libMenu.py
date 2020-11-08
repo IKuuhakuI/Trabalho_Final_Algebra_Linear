bbb1de09e832b7ac358d3f54c6791487b1a124a4
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2020-11-08 13:47:33 -0300

    adiciona a funcao que verifica a opcao selecionada para a libMenu

--- libMenu.py
+++ libMenu.py
@@ -6,3 +6,25 @@ def MostrarMenu ():
 	print ("3- Calcular modelo linear para a Iris-virginica")
 	print ("O- Sair do programa")
 	print ()
+
+def VerificarOpcao (opcaoSelecionada):
+
+	criarModeloLinear = True
+
+	if (opcaoSelecionada == '0'):
+		criarModeloLinear = False
+
+	elif (opcaoSelecionada == '1'):
+		print (1)
+
+	elif (opcaoSelecionada == '2'):
+		print (2)
+
+	elif (opcaoSelecionada == '3'):
+		print (3)
+
+	else:
+		print ("Opcao invalida!")
+
+	return criarModeloLinear
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from libFormatar import LerCsv, SepararTipos, FormatarMatriz
 from libMatriz import GerarMatrizTransposta, MultiplicarMatrizes, MostrarMatriz
-from libMenu import MostrarMenu
+from libMenu import MostrarMenu, VerificarOpcao
 
 def main ():
 	# Controles do Menu
@@ -48,20 +48,7 @@ def main ():
 		opcaoSelecionada = input ("Selecione a sua opcao: ")
 		print ()
 
-		if (opcaoSelecionada == '0'):
-			criarModeloLinear = False
-
-		elif (opcaoSelecionada == '1'):
-			print (1)
-
-		elif (opcaoSelecionada == '2'):
-			print (2)
-
-		elif (opcaoSelecionada == '3'):
-			print (3)
-
-		else:
-			print ("Opcao invalida!")
+		criarModeloLinear = VerificarOpcao (opcaoSelecionada)
 
 # Chamada da main
 main ()
